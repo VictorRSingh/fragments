176213b5cc6ffa58c366dfedd8756622a8a663e8
vsingh57 <vsingh57@myseneca.ca>
2025-09-12 19:13:37 -0400

    Add debug log in server.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ const logger = require('./logger');
 const app = require('./app');
 const port = parseInt(process.env.PORT || '8080', 10);
 
+// Print Environment Variables in debug mode
+if (process.env.LOG_LEVEL === 'debug') {
+  console.log('Environment Variables:', process.env);
+}
+
 const server = stoppable(
   app.listen(port, () => {
     logger.info(`Server started on port ${port}`);
